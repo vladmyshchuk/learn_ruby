6a73106e83d68c78736bc8c794c6abe0ad6048a2
Vlad Myshchuk <vladmyshchuk@gmail.com>
2020-01-22 11:09:39 -0500

    defined ftoc and ctof methods to fix all errors

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,13 @@
-#write your code here
+# frozen_string_literal: true
+
+# write your code here
+
+def ftoc(fahrenheit_degrees)
+  celsius = (fahrenheit_degrees.to_f - 32.0) * 5.0 / 9.0
+  celsius.round(3)
+  end
+
+def ctof(celsius_degrees)
+  fahrenheit = (celsius_degrees.to_f * 9.0 / 5.0) + 32.0
+  fahrenheit.round(3)
+end
