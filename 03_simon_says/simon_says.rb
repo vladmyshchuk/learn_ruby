a8ab7713886d232a59a5da520a71950b287e9ccd
vladmyshchuk <vladmyshchuk@gmail.com>
2020-02-04 14:46:35 -0500

    Fixed 03_simon_says to run test successfully & currently trying to resolve the last part of 04_pig_latin

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -2,12 +2,8 @@
 
 # write your code here
 
-def echo(x)
-  if x == 'hello'
-    'hello'
-  elsif x == 'bye'
-    'bye'
-  end
+def echo(s)
+  s
 end
 
 def shout(x)
@@ -18,6 +14,22 @@ def repeat(x, y = 2)
   ([x] * y).join(' ')
 end
 
-def start_of_word(word, n)
-  word[n - 1].chars
+def start_of_word(x, n)
+  x[0...n]
+end
+
+def first_word(x)
+  x.split(" ").first
+end
+
+def titleize(x)
+  words = x.split.map do |word|
+    if %w(the and over).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(" ")
 end
